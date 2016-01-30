d9789474f2fb9749a652c32229c293b58d3eaa00
pianist1119 <pianist1119@gmail.com>
2016-01-30 13:16:35 -0500

    Added constants for setpoints and arm/screw pids. added the initialization of member components.

--- src/Arm.cpp
+++ src/Arm.cpp
@@ -1,11 +1,35 @@
 #include <Arm.h>
 
 Arm::Arm(HotBot* bot) : HotSubsystem(bot, "Arm") {
-	// TODO Auto-generated constructor stub
+/*
+ * Super simple init code. see Arm.h for constant declarations
+ */
+	m_armLeftTalon = new CANTalon(ARM_ID_LEFT);
+	m_armRightTalon = new CANTalon(ARM_ID_RIGHT);
 
+	m_screwRightTalon = new CANTalon(SCREW_DRIVE_ID_RIGHT);
+	m_screwLeftTalon = new CANTalon(SCREW_DRIVE_ID_LEFT);
+
+	m_screwEncoder = new Encoder(ENCODER_CHANNEL1_SCREW,ENCODER_CHANNEL2_SCREW);
+	m_armEncoder = new Encoder(ENCODER_CHANNEL1_ARM,ENCODER_CHANNEL2_ARM);
+
+	m_armPIDController = new PIDController(ARM_P,ARM_I,ARM_D,m_armEncoder,m_armLeftTalon);
+
+	m_screwPIDController = new PIDController(SCREW_P,SCREW_I,SCREW_D,m_screwEncoder,m_screwLeftTalon);
+/*
+ * Slave the right motors to the left ones which will be controlled by PIDs and Teleop
+ */
+	m_screwRightTalon->SetControlMode(CANSpeedController::kFollower); //Slavery is good
+	m_screwRightTalon->Set(SCREW_DRIVE_ID_LEFT);
+
+	m_armRightTalon->SetControlMode(CANSpeedController::kFollower); // See Above
+	m_armRightTalon->Set(ARM_ID_LEFT);
 }
 
 Arm::~Arm() {
-	// TODO Auto-generated destructor stub
+	// TODO Auto-generated destructor stub // i thinks me will leave this heres
 }
 
+void Arm::SetExtend(float speed) {
+
+}
--- src/Arm.h
+++ src/Arm.h
@@ -2,39 +2,76 @@
 #define SRC_ARM_H_
 
 #include <RobotUtils/HotSubsystem.h>
-#include <string>
 
-const enum arm_pid {
-	far_high_goal = 1,
-	climb = 2,
-	medium_low_goal = 3, //I think. 50 degrees pulse roller for .1 seconds
-	close_high_goal = 4,
-	carry = 5,
-	close_low_goal = 6,
-	pickup = 7,
-	retract_screw = 8,
-	obstacles = 9
+#define ARM_P 0
+#define ARM_I 0
+#define ARM_D 0
+#define SCREW_P 0
+#define SCREW_I 0
+#define SCREW_D 0
+
+#define FAR_HIGH_GOAL 0
+#define CLIMB 0
+#define MEDIUM_LOW_GOAL 0
+#define CLOSE_HIGH_GOAL 0
+#define CARRY 0
+#define CLOSE_LOW_GOAL 0
+#define PICKUP 0
+#define RETRACT_SCREW 0
+#define OBSTACLE 0
+
+#define ENCODER_CHANNEL1_ARM 4
+#define ENCODER_CHANNEL2_ARM 5
+#define ENCODER_CHANNEL1_SCREW 6
+#define ENCODER_CHANNEL2_SCREW 7
+
+#define SCREW_DRIVE_ID_LEFT 14
+#define SCREW_DRIVE_ID_RIGHT 15
+#define ARM_ID_RIGHT 12
+#define ARM_ID_LEFT 11
+
+enum ArmSetPoint {
+	kFarHighGoal = 1,
+	kClimb = 2,
+	kMediumLowGoal = 3, //I think. 50 degrees pulse roller for .1 seconds
+	kCloseHighGoal = 4,
+	kCarry = 5,
+	kCloseLowgoal = 6,
+	kPickup = 7,
+	kRetractScrew = 8,
+	kObstacle= 9
 };
 
-
 class Arm: public HotSubsystem {
+
+	CANTalon* m_armLeftTalon;
+	CANTalon* m_armRightTalon;
+
+	CANTalon* m_screwLeftTalon;
+	CANTalon* m_screwRightTalon;
+
+	Encoder* m_screwEncoder;
+	Encoder* m_armEncoder;
+
+	PIDController* m_armPIDController;
+	PIDController* m_screwPIDController;
+
 public:
 
-	Arm(HotBot* bot);
+	Arm(HotBot* bot); //Constructor
 	virtual ~Arm();
 
 	void Set(float speed); //Set the Speed of the Arm
 	void SetExtend(float speed); //Set the Speed of the Screw Drive / Arm Extender
 
-	void SetPIDPoint(arm_pid controller, double value); //Set the desired pidcontroller setpoint, such as close_low_goal
+	void SetPIDPoint(ArmSetPoint setpoint); //Set the desired pidcontroller setpoint, such as close_low_goal
 
-	void EnablePID(); //Enable the Pids
+	void EnableArmPID(); //Enable the Pids
+	void DisableArmPID(); //Disable the Pids
 
-	void DisablePID(); //Disable the Pids
+	void EnableScrewPID();
+	void DisableScrewPID();
 
-	void EnablePID(arm_pid controller); //Enable a specific pid controller
-
-	void DisablePID(arm_pid controller); //Disable a specific pid controller
 };
 
 #endif /* SRC_ARM_H_ */
