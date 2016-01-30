d3452c97bc2ed8c3b8196a45f6b3cff77678e582
jyori112 <jyori112@gmail.com>
2016-01-30 13:34:49 -0500

    Create camera handler class

---
+++ src/CameraHandler.cpp
@@ -0,0 +1,18 @@
+/*
+ * CameraHandler.cpp
+ *
+ *  Created on: Jan 30, 2016
+ *      Author: Robo13
+ */
+
+#include <CameraHandler.h>
+
+CameraHandler::CameraHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CameraHandler::~CameraHandler() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/CameraHandler.h
@@ -0,0 +1,17 @@
+/*
+ * CameraHandler.h
+ *
+ *  Created on: Jan 30, 2016
+ *      Author: Robo13
+ */
+
+#ifndef SRC_CAMERAHANDLER_H_
+#define SRC_CAMERAHANDLER_H_
+
+class CameraHandler {
+public:
+	CameraHandler();
+	virtual ~CameraHandler();
+};
+
+#endif /* SRC_CAMERAHANDLER_H_ */
