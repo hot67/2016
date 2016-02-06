4abe911f9652ed03c7ed0cacc5a1028b0ab11900
pianist1119 <pianist1119@gmail.com>
2016-02-06 08:15:44 -0500

    Removed Extraneous Files

--- src/potentiometer.cpp
+++
@@ -1,9 +0,0 @@
-#include "WPILib.h"
-#include <AnalogPotentiometer.h>
-
-int main() {
-	AnalogPotentiometer IDEK(1);
-	int x = IDEK.Get();
-
-	SmartDashboard::PutNumber("AnalogPotentiometer", x);
-}
